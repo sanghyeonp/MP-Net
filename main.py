2c3414c2a50e5b6cc69e64c55463191931ee77c3
sangsta7 <sanghyeon.park@ghent.ac.kr>
2021-06-14 19:30:35 +0900

    update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 # unet arch. and evaluation metrics from https://github.com/qubvel/segmentation_models.pytorch
 # Accuracy in segmentation_models.segmentation_models_pytorch.utils.metrics.py was modified to calculate balanced accuracy
 import code
+import sys
 import os
 import csv
 from datetime import datetime
 from tqdm import tqdm
+from src.logging import Logger
 import torch
 import torch.nn as nn
 from torch.nn import Conv2d, BatchNorm2d
@@ -35,9 +37,68 @@ def main(model, train=True, weights=None, test=True, optimizer='adam', momentum=
 
 	optimizer : choose optimization method from either 'adam', 'sgd'
 	"""
-	
 	torch.manual_seed(0)
 	model_name, loss_name, optim_name = model, criterion, optimizer
+	TTA_is = False
+	if TTA is not False:
+		TTA_is = True
+
+	time = datetime.now()
+	t = time.strftime("%Y-%m-%d-%H-%M-%S")
+	if train:
+		filename = '{}_[{}]_[{}]_[{}]_[{}_{}]_TTA[{}]'.format(t, model_name, criterion, optimizer, epoch, lr, TTA_is)
+	else:
+		filename = '{}_[{}]_pretrained[{}]_TTA[{}]'.format(t, model_name, weights.split(sep='/')[-1], TTA_is)
+
+	if not os.path.exists(os.path.join(os.getcwd(), 'result')):
+		os.mkdir(os.path.join(os.getcwd(), 'result'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'train_result')):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'train_result'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'train_result', model_name)):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'train_result', model_name))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'model_saved')):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'model_saved'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'model_saved', model_name)):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'model_saved', model_name))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred_mask')):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'pred_mask'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred_mask', model_name)):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'pred_mask', model_name))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred_mask', model_name, filename)):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'pred_mask', model_name, filename))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'evaluation')):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'evaluation'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'evaluation', model_name)):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'evaluation', model_name))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'stdout')):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'stdout'))
+	if not os.path.exists(os.path.join(os.getcwd(), 'result', 'stdout', model_name)):
+		os.mkdir(os.path.join(os.getcwd(), 'result', 'stdout', model_name))
+
+	sys.stdout = Logger(os.path.join(os.getcwd(), 'result', 'stdout', model_name, filename+'.txt'))
+
+	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+	evaluation_metrics = [	Accuracy(balanced=True, threshold=None).to(device), Recall(threshold=None).to(device), 
+							Precision(threshold=None).to(device), Fscore(threshold=None).to(device), IoU(threshold=None).to(device)]
+	# Initialize model
+	if model_name == 'unet':		# Initialize U-Net model
+		model = smp.Unet(encoder_name="resnet101", in_channels=3, classes=1, encoder_weights="imagenet")
+		evaluate, train_model, testset_evaluation = evaluate_unet, train_unet, testset_evaluation_unet
+	elif model_name == 'fcn':	# Initialize FCN model
+		model = fcn_resnet101(pretrained=True)
+		# The last convolutional layer is modified to produce a binary output.
+		model.classifier._modules['4'] = Conv2d(512, 1, kernel_size=(1, 1), stride=(1, 1))
+		model.aux_classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
+		evaluate, train_model, testset_evaluation = evaluate_torchvision, train_torchvision, testset_evaluation_torchvision
+	elif model_name == 'deeplabv3':	# Initialize Deeplabv3 model
+		model = deeplabv3_resnet101(pretrained=True)
+		# The last convolutional layer is modified to produce a binary output.
+		model.classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
+		model.aux_classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
+		evaluate, train_model, testset_evaluation = evaluate_torchvision, train_torchvision, testset_evaluation_torchvision
+	elif model_name == 'nested_unet':
+		model = NestedUNet(num_classes=1, input_channels=3, deep_supervision=False)
+		evaluate, train_model, testset_evaluation = evaluate_unet, train_unet, testset_evaluation_unet
 
 	if train:
 		# Create saving location
@@ -45,54 +106,13 @@ def main(model, train=True, weights=None, test=True, optimizer='adam', momentum=
 		train log: saves losses and validation scores for every epochs
 		saved model: saves the model with lowest validation loss
 		"""
-		time = datetime.now()
-		t = time.strftime("%Y%m%d_%H%M%S")
-		print("Initiating... Model [{}] Loss function [{}] Optimizer [{}]".format(model_name, loss_name, optim_name))
 
-		if not os.path.exists(os.path.join(os.getcwd(), 'result')):
-			os.mkdir(os.path.join(os.getcwd(), 'result'))
-		if not os.path.exists(os.path.join(os.getcwd(), 'result', 'train_log')):
-			os.mkdir(os.path.join(os.getcwd(), 'result', 'train_log'))
-		if not os.path.exists(os.path.join(os.getcwd(), 'result', 'train_log', model_name)):
-			os.mkdir(os.path.join(os.getcwd(), 'result', 'train_log', model_name))
-		if not os.path.exists(os.path.join(os.getcwd(), 'result', 'saved_model')):
-			os.mkdir(os.path.join(os.getcwd(), 'result', 'saved_model'))
-		if not os.path.exists(os.path.join(os.getcwd(), 'result', 'saved_model', model_name)):
-			os.mkdir(os.path.join(os.getcwd(), 'result', 'saved_model', model_name))
-		
-		TTA_is = False
-		if TTA is not False:
-			TTA_is = True
-		
-		filename = '{}_model[{}]_loss[{}]_optim[{}]_epoch[{}]_TTA[{}]'.format(t, model_name, criterion, optimizer, epoch, TTA_is)
+		print("Initiating... Model [{}] Loss function [{}] Optimizer [{}]".format(model_name, loss_name, optim_name))		
 		print("File name [{}]".format(filename))
-		with open(os.path.join(os.getcwd(), 'result', 'train_log', model_name, filename+'.csv'), 'wt', newline='') as f1:
+
+		with open(os.path.join(os.getcwd(), 'result', 'train_result', model_name, filename+'.csv'), 'wt', newline='') as f1:
 			f1_writer = csv.writer(f1)
 			f1_writer.writerow(['Epoch', 'Train loss', 'Val loss', 'Accuracy', 'Recall', 'Precision', 'F1-score', 'IoU'])
-
-			device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-			evaluation_metrics = [	Accuracy(balanced=True, threshold=None).to(device), Recall(threshold=None).to(device), 
-									Precision(threshold=None).to(device), Fscore(threshold=None).to(device), IoU(threshold=None).to(device)]
-
-			# Initialize model
-			if model_name == 'unet':		# Initialize U-Net model
-				model = smp.Unet(encoder_name="resnet101", in_channels=3, classes=1, encoder_weights="imagenet")
-				evaluate, train_model = evaluate_unet, train_unet
-			elif model_name == 'fcn':	# Initialize FCN model
-				model = fcn_resnet101(pretrained=True)
-				# The last convolutional layer is modified to produce a binary output.
-				model.classifier._modules['4'] = Conv2d(512, 1, kernel_size=(1, 1), stride=(1, 1))
-				model.aux_classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
-				evaluate, train_model = evaluate_torchvision, train_torchvision
-			elif model_name == 'deeplabv3':	# Initialize Deeplabv3 model
-				model = deeplabv3_resnet101(pretrained=True)
-				# The last convolutional layer is modified to produce a binary output.
-				model.classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
-				model.aux_classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
-				evaluate, train_model = evaluate_torchvision, train_torchvision
-			elif model_name == 'nested_unet':
-				model = NestedUNet(num_classes=1, input_channels=3, deep_supervision=False)
-				evaluate, train_model = evaluate_unet, train_unet
 
 			# Initialize optimizer
 			if optimizer == 'adam':
@@ -121,127 +141,81 @@ def main(model, train=True, weights=None, test=True, optimizer='adam', momentum=
 
 			print("Time: {}".format(time))
 			print("Start training...\n")
-			val_loss, val_performances = evaluate(	model=model, device=device, total_epoch=epoch, epoch=0, val_loader=val_loader, 
-													activation=activation, criterion=criterion, metrics=evaluation_metrics
+			val_loss, val_performances = evaluate(	model=model, device=device, total_epoch=epoch, epoch=0, val_loader=val_loader, \
+													activation=activation, criterion=criterion, metrics=evaluation_metrics \
 													)
 			f1_writer.writerow([0, 'NA', val_loss] +  val_performances)
 
-			best_epoch, evaluations = train_model(	model=model, device=device, total_epoch=epoch, 
-													train_loader=train_loader, val_loader=val_loader, 
-													criterion=criterion, optimizer=optimizer, metrics=evaluation_metrics, 
-													activation=activation,	writer=f1_writer, filename=filename,
-													save2=os.path.join(os.getcwd(), 'result', 'saved_model', model_name)
+			best_epoch, evaluations = train_model(	model=model, device=device, total_epoch=epoch,\
+													train_loader=train_loader, val_loader=val_loader, \
+													criterion=criterion, optimizer=optimizer, metrics=evaluation_metrics, \
+													activation=activation,	writer=f1_writer, filename=filename, \
+													save2=os.path.join(os.getcwd(), 'result', 'model_saved', model_name)\
 													)
 			print("\nFinished training...")
 			print("Time: {}\n".format(datetime.now()))
 			print("Training result for [{}]:".format(filename))
 			tqdm.write("Model saved at best epoch [{}]\nValidation loss [{:.4f}]\nAccuracy [{:.4f}]\nRecall [{:.4f}]\nPrecision [{:.4f}]\
-						\nF1-score [{:.4f}]\nIoU [{:.4f}]\n".format(best_epoch, evaluations[0], evaluations[1], evaluations[2],
-																	evaluations[3], evaluations[4], evaluations[5]
-																	)
+						\nF1-score [{:.4f}]\nIoU [{:.4f}]\n".format(best_epoch, evaluations[0], evaluations[1], evaluations[2], \
+																	evaluations[3], evaluations[4], evaluations[5] \
+																	) \
 						)
 		
 		if test:	# Train 및 test 다 True 일 때는, training 할때 save된 parameter로 test 하기
 			print("Evaluating...")
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred')):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'pred'))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred', model_name)):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'pred', model_name))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred', model_name, filename)):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'pred', model_name, filename))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'testset_evaluation')):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'testset_evaluation'))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'testset_evaluation', model_name)):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'testset_evaluation', model_name))
 
-			if model_name == 'unet' or model_name == 'nested_unet':
-				testset_evaluation = testset_evaluation_unet
-			elif model_name == 'fcn' or model_name == 'deeplabv3':
-				testset_evaluation = testset_evaluation_torchvision
-
-			with open(os.path.join(os.getcwd(), 'result', 'testset_evaluation', model_name, filename+'.csv'), 'wt', newline='') as f2:
+			with open(os.path.join(os.getcwd(), 'result', 'evaluation', model_name, filename+'.csv'), 'wt', newline='') as f2:
 				f2_writer =csv.writer(f2)
 				f2_writer.writerow(['Image number', 'Accuracy', 'Recall', 'Precision', 'F1-score', 'IoU', '', 'TP', 'FP', 'FN', 'TN'])
-				performances, confusion = testset_evaluation(	model=model, device=device, testset_path=os.path.join(os.getcwd(), 'dataset', 'test'), 
-																weight=os.path.join(os.getcwd(), 'result', 'saved_model', model_name, filename+'.pth'), metrics=evaluation_metrics, 
-																save2=os.path.join(os.getcwd(), 'result', 'pred', model_name, filename), write2=f2_writer, TTA=TTA
+				performances, confusion = testset_evaluation(	model=model, device=device, testset_path=os.path.join(os.getcwd(), 'dataset', 'test'), \
+																weight=os.path.join(os.getcwd(), 'result', 'model_saved', model_name, filename+'.pth'), \
+																metrics=evaluation_metrics, save2=os.path.join(os.getcwd(), 'result', 'pred_mask', model_name, filename), \
+																write2=f2_writer, TTA=TTA \
 																)
 			print("Finished evaluation...\n")
 			print("Evaluation result for [{}]:".format(filename))
 			print("Accuracy [{:.4f}]\nRecall [{:.4f}]\nPrecision [{:.4f}]\nF1-score [{:.4f}]\nIoU [{:.4f}]\
-					\nTP [{}] | FP [{}] | FN [{}] | TN [{}]\n".format(performances[0], performances[1], performances[2], performances[3], performances[4],
-																	confusion[0], confusion[1], confusion[2], confusion[3]))
+					\nTP [{}] | FP [{}] | FN [{}] | TN [{}]\n".format(performances[0], performances[1], performances[2], performances[3], performances[4], \
+																	confusion[0], confusion[1], confusion[2], confusion[3]) \
+					)
 
 	else:	# If training is not being done. Load saved model.
 		if test:
 			if weights is None:
 				raise AttributeError("Need to provide pre-trained weight if performing only testing.")
-			
-			t = datetime.now().strftime("%Y%m%d_%H%M%S")
-			device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-			evaluation_metrics = [	Accuracy(balanced=True, threshold=None).to(device), Recall(threshold=None).to(device), 
-									Precision(threshold=None).to(device), Fscore(threshold=None).to(device), IoU(threshold=None).to(device)]
-
-			TTA_is = False
-			if TTA is not False:
-				TTA_is = True
-
-			filename = '{}_model[{}]_pretrained[{}]_TTA[{}]'.format(t, model_name, weights.split(sep='/')[-1], TTA_is)
-
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred')):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'pred'))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred', model_name)):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'pred', model_name))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'pred', model_name, filename)):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'pred', model_name, filename))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'testset_evaluation')):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'testset_evaluation'))
-			if not os.path.exists(os.path.join(os.getcwd(), 'result', 'testset_evaluation', model_name)):
-				os.mkdir(os.path.join(os.getcwd(), 'result', 'testset_evaluation', model_name))
-
-			if model_name == 'unet' or model_name == 'nested_unet':
-				if model_name == 'unet':
-					model = smp.Unet(encoder_name="resnet101", in_channels=3, classes=1, encoder_weights=None)
-				elif model_name == 'nested_unet':
-					model = NestedUNet(num_classes=1, input_channels=3, deep_supervision=False)
-				testset_evaluation = testset_evaluation_unet
-			elif model_name == 'fcn' or model_name == 'deeplabv3':
-				if model_name == 'fcn':
-					model = fcn_resnet101(pretrained=True)
-					# The last convolutional layer is modified to produce a binary output.
-					model.classifier._modules['4'] = Conv2d(512, 1, kernel_size=(1, 1), stride=(1, 1))
-					model.aux_classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
-				elif model_name == 'deeplabv3':
-					model = deeplabv3_resnet101(pretrained=True)
-					# The last convolutional layer is modified to produce a binary output.
-					model.classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
-					model.aux_classifier._modules['4'] = Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
-				testset_evaluation = testset_evaluation_torchvision
 
 			print("Evaluating...")
-			with open(os.path.join(os.getcwd(), 'result', 'testset_evaluation', model_name, filename+'.csv'), 'wt', newline='') as f2:
+			with open(os.path.join(os.getcwd(), 'result', 'evaluation', model_name, filename+'.csv'), 'wt', newline='') as f2:
 				f2_writer =csv.writer(f2)
 				f2_writer.writerow(['Image number', 'Accuracy', 'Recall', 'Precision', 'F1-score', 'IoU', '', 'TP', 'FP', 'FN', 'TN'])
-				performances, confusion = testset_evaluation(	model=model, device=device, testset_path=os.path.join(os.getcwd(), 'dataset', 'test'), 
-																weight=os.path.join(weights), metrics=evaluation_metrics, 
-																save2=os.path.join(os.getcwd(), 'result', 'pred', model_name, filename), write2=f2_writer, TTA=TTA
+				performances, confusion = testset_evaluation(	model=model, device=device, testset_path=os.path.join(os.getcwd(), 'dataset', 'test'), \
+																weight=weights, metrics=evaluation_metrics, \
+																save2=os.path.join(os.getcwd(), 'result', 'pred_mask', model_name, filename), \
+																write2=f2_writer, TTA=TTA \
 																)
 			print("Finished evaluation...\n")
 			print("Evaluation result for [{}]".format(filename))
 			print("Accuracy [{:.4f}]\nRecall [{:.4f}]\nPrecision [{:.4f}]\nF1-score [{:.4f}]\nIoU [{:.4f}]\
-					\nTP [{}] | FP [{}] | FN [{}] | TN [{}]\n".format(performances[0], performances[1], performances[2], performances[3], performances[4],
-																	confusion[0], confusion[1], confusion[2], confusion[3]))
-
-
-	# with open(os.path.join(os.getcwd(), 'train_log', ))
+					\nTP [{}] | FP [{}] | FN [{}] | TN [{}]\n".format(performances[0], performances[1], performances[2], performances[3], performances[4], \
+																	confusion[0], confusion[1], confusion[2], confusion[3]) \
+					)
 
 
 if __name__ == '__main__':
+	################################### INPUTS ###################################
+	model = 'unet'
+	train=True
+	# weights=None
+	weights="/home/sangp/bachelor_thesis/git_repo/microplastics/result/model_saved/unet/2021-06-14-19-23-04_[unet]_[dice]_[adam]_[2_0.001]_TTA[False].pth"
+	test=True
+	epoch=2
+	batch_size=10
+	criterion='dice'
+	optimizer='adam'
+	momentum=0.9
+	lr=0.001
+	TTA=False
+	##############################################################################
+	main(model=model, train=train, weights=weights, test=test, epoch=epoch, batch_size=batch_size, criterion=criterion, optimizer=optimizer, momentum=momentum, lr=lr, TTA=TTA)
 
-	# main(model='unet', train=True, weights=None, test=True, optimizer='sgd', momentum=0.9, lr=0.001, epoch=15, batch_size=10, criterion='dice', TTA=False)
-	main(model='unet', train=False, weights="/home/sangp/bachelor_thesis/git_repo/microplastics/result/saved_model/unet/20210613_192714_model[unet]_loss[dice]_optim[sgd]_epoch[15]_TTA[False].pth", 
-		test=True, optimizer='adam', momentum=0.9, lr=0.001, epoch=20, batch_size=10, criterion='dice', TTA=False)
-	# main(model='fcn', epoch=5)
-	# main(model='deeplabv3', epoch=5)
-	# main(model='nested_unet', epoch=5)
 	# code.interact(local=dict(globals(), **locals()))
